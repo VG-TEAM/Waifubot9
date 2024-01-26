7b341413f96f401ee3b3c9fb0b08bbee2b558e18
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-01-26 18:52:32 +0530

    Update __main__.py

--- Grabber/__main__.py
+++ Grabber/__main__.py
@@ -44,62 +44,62 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
     lock = locks[chat_id]
 
     async with lock:
-        
+
         chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
         if chat_frequency:
             message_frequency = chat_frequency.get('message_frequency', 100)
         else:
             message_frequency = 100
 
-        
+
         if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
             last_user[chat_id]['count'] += 1
             if last_user[chat_id]['count'] >= 10:
-            
+
                 if user_id in warned_users and time.time() - warned_users[user_id] < 600:
                     return
                 else:
-                    
+
                     await update.message.reply_text(f"⚠️ Don't Spam {update.effective_user.first_name}...\nYour Messages Will be ignored for 10 Minutes...")
                     warned_users[user_id] = time.time()
                     return
         else:
             last_user[chat_id] = {'user_id': user_id, 'count': 1}
 
-    
+
         if chat_id in message_counts:
             message_counts[chat_id] += 1
         else:
             message_counts[chat_id] = 1
 
-    
+
         if message_counts[chat_id] % message_frequency == 0:
             await send_image(update, context)
-            
+
             message_counts[chat_id] = 0
-            
+
 async def send_image(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
 
 
     all_characters = list(await collection.find({}).to_list(length=None))
-    
-    
+
+
     if chat_id not in sent_characters:
         sent_characters[chat_id] = []
 
-    
+
     if len(sent_characters[chat_id]) == len(all_characters):
         sent_characters[chat_id] = []
 
-    
+
     character = random.choice([c for c in all_characters if c['id'] not in sent_characters[chat_id]])
 
-    
+
     sent_characters[chat_id].append(character['id'])
     last_characters[chat_id] = character
 
-    
+
     if chat_id in first_correct_guesses:
         del first_correct_guesses[chat_id]
 
@@ -109,7 +109,7 @@ async def send_image(update: Update, context: CallbackContext) -> None:
         photo=character['img_url'],
         caption=f"""A New {character['rarity']} Character Appeared...\n/guess Character Name and add in Your Harem""",
         parse_mode='Markdown')
-    
+
 async def guess(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
@@ -122,7 +122,7 @@ async def guess(update: Update, context: CallbackContext) -> None:
         return
 
     guess = ' '.join(context.args).lower() if context.args else ''
-    
+
     if "()" in guess or "&" in guess.lower():
         await update.message.reply_text("Nahh You Can't use This Types of words in your guess..❌️")
         return
@@ -132,9 +132,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
 
     if sorted(name_parts) == sorted(guess.split()) or any(part == guess for part in name_parts):
 
-    
+
         first_correct_guesses[chat_id] = user_id
-        
+
         user = await user_collection.find_one({'id': user_id})
         if user:
             update_fields = {}
@@ -144,9 +144,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 update_fields['first_name'] = update.effective_user.first_name
             if update_fields:
                 await user_collection.update_one({'id': user_id}, {'$set': update_fields})
-            
+
             await user_collection.update_one({'id': user_id}, {'$push': {'characters': last_characters[chat_id]}})
-      
+
         elif hasattr(update.effective_user, 'username'):
             await user_collection.insert_one({
                 'id': user_id,
@@ -155,7 +155,7 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 'characters': [last_characters[chat_id]],
             })
 
-        
+
         group_user_total = await group_user_totals_collection.find_one({'user_id': user_id, 'group_id': chat_id})
         if group_user_total:
             update_fields = {}
@@ -165,9 +165,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 update_fields['first_name'] = update.effective_user.first_name
             if update_fields:
                 await group_user_totals_collection.update_one({'user_id': user_id, 'group_id': chat_id}, {'$set': update_fields})
-            
+
             await group_user_totals_collection.update_one({'user_id': user_id, 'group_id': chat_id}, {'$inc': {'count': 1}})
-      
+
         else:
             await group_user_totals_collection.insert_one({
                 'user_id': user_id,
@@ -178,7 +178,7 @@ async def guess(update: Update, context: CallbackContext) -> None:
             })
 
 
-    
+
         group_info = await top_global_groups_collection.find_one({'group_id': chat_id})
         if group_info:
             update_fields = {}
@@ -186,9 +186,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 update_fields['group_name'] = update.effective_chat.title
             if update_fields:
                 await top_global_groups_collection.update_one({'group_id': chat_id}, {'$set': update_fields})
-            
+
             await top_global_groups_collection.update_one({'group_id': chat_id}, {'$inc': {'count': 1}})
-      
+
         else:
             await top_global_groups_collection.insert_one({
                 'group_id': chat_id,
@@ -197,7 +197,7 @@ async def guess(update: Update, context: CallbackContext) -> None:
             })
 
 
-        
+
         keyboard = [[InlineKeyboardButton(f"See Harem", switch_inline_query_current_chat=f"collection.{user_id}")]]
 
 
@@ -205,18 +205,18 @@ async def guess(update: Update, context: CallbackContext) -> None:
 
     else:
         await update.message.reply_text('Please Write Correct Character Name... ❌️')
-   
+
 async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
 
-    
+
     if not context.args:
         await update.message.reply_text('Please provide Character id...')
         return
 
     character_id = context.args[0]
 
-    
+
     user = await user_collection.find_one({'id': user_id})
     if not user:
         await update.message.reply_text('You have not Guessed any characters yet....')
@@ -228,14 +228,14 @@ async def fav(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text('This Character is Not In your collection')
         return
 
-    
+
     user['favorites'] = [character_id]
 
-    
+
     await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}})
 
     await update.message.reply_text(f'Character {character["name"]} has been added to your favorite...')
-    
+
 
 def main() -> None:
     """Run bot."""
@@ -244,9 +244,8 @@ def main() -> None:
     application.add_handler(CommandHandler("fav", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
     application.run_polling(drop_pending_updates=True)
-    
+
 if __name__ == "__main__":
     Grabberu.start()
     LOGGER.info("Bot started")
     main()
-
