3cd8701898cfdb86ae4a4b26296ccd7fa3fef733
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-01-26 17:11:43 +0530

    Update start.py

--- Grabber/modules/start.py
+++ Grabber/modules/start.py
@@ -4,7 +4,7 @@ from html import escape
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 
-from Grabber import application, PHOTO_URL, SUPPORT_CHAT, UPDATE_CHAT, BOT_USERNAME, db, GROUP_ID
+from Grabber import application, db, GROUP_ID, BOT_USERNAME, SUPPORT_CHAT, UPDATE_CHAT
 
 collection = db['total_pm_users']
 
@@ -12,56 +12,52 @@ async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     first_name = update.effective_user.first_name
     username = update.effective_user.username
+    chat_id = update.effective_chat.id
 
     user_data = await collection.find_one({"_id": user_id})
 
     if user_data is None:
-        
         await collection.insert_one({"_id": user_id, "first_name": first_name, "username": username})
-        
-        await context.bot.send_message(chat_id=GROUP_ID, 
-                                       text=f"New user Started The Bot..\n User: <a href='tg://user?id={user_id}>{escape(first_name)})</a>", 
+
+        # Send information to the specified group
+        await context.bot.send_message(chat_id=GROUP_ID,
+                                       text=f"New user started the bot in group {chat_id}.\n"
+                                            f"User: <a href='tg://user?id={user_id}>{escape(first_name)}</a>",
                                        parse_mode='HTML')
-    else:
-        
+
+    # Update the existing code below...
+    if user_data is not None:
         if user_data['first_name'] != first_name or user_data['username'] != username:
-            
             await collection.update_one({"_id": user_id}, {"$set": {"first_name": first_name, "username": username}})
 
-    
+    if update.effective_chat.type == "private":
+        photo_url = "https://telegra.ph/file/131faebb4ad2d79281c4d.jpg"  # Your photo URL
 
-    if update.effective_chat.type== "private":
-        
-        
-        caption = f"""
+        caption = """
         ***Heyyyy...***
 
-***I am An {BOT_USERNAME}  Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***I am An Open Source Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After every 100 messages in the Group... Use /guess to Collect those Characters in Your Collection... and see your Collection by using /Harem... So add me to your groups and start collecting your harem!***
         """
         keyboard = [
-            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴅ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-             [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
+             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        photo_url = random.choice(PHOTO_URL)
 
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
 
     else:
-        photo_url = random.choice(PHOTO_URL)
+        photo_url = "https://telegra.ph/file/131faebb4ad2d79281c4d.jpg"  # Your photo URL
         keyboard = [
-            
             [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("ᴜᴏᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
+             InlineKeyboardButton("ᴜᴏᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("ᴅᴇᴠ", url=f'http://t.me/iam_daxx')],
-            
         ]
-        
+
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption="ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ",reply_markup=reply_markup )
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption="ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ", reply_markup=reply_markup)
 
 async def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -100,7 +96,6 @@ async def button(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
-            [InlineKeyboardButton("ᴅᴇᴠ", url=f'https://github.com/DAXXTEAM')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
