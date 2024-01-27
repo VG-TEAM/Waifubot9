2d2e2cf301478cb4491cb1ee5a3c0950167418b9
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-01-27 10:09:55 +0530

    Update leaderboard.py

--- Grabber/modules/leaderboard.py
+++ Grabber/modules/leaderboard.py
@@ -13,26 +13,26 @@ from Grabber import sudo_users as SUDO_USERS
 
     
 async def global_leaderboard(update: Update, context: CallbackContext) -> None:
-    
+   
     cursor = top_global_groups_collection.aggregate([
         {"$project": {"group_name": 1, "count": 1}},
         {"$sort": {"count": -1}},
-        {"$limit": 10}
+        {"$limit": 20}
     ])
-    leaderboard_data = await cursor.to_list(length=10)
+    leaderboard_data = await cursor.to_list(length=20)
 
-    leaderboard_message = "<b>TOP 10 GROUPS WHO GUESSED MOST CHARACTERS</b>\n\n"
+    leaderboard_message = "<b>TOP 20 GROUPS WHO GUESSED MOST CHARACTERS</b>\n\n"
 
     for i, group in enumerate(leaderboard_data, start=1):
         group_name = html.escape(group.get('group_name', 'Unknown'))
 
-        if len(group_name) > 10:
-            group_name = group_name[:15] + '...'
+        if len(group_name) > 20:
+            group_name = group_name[:25] + '...'
         count = group['count']
         leaderboard_message += f'{i}. <b>{group_name}</b> ➾ <b>{count}</b>\n'
     
     
-    photo_url = random.choice(PHOTO_URL)
+    photo_url = "https://telegra.ph/file/1d9c963d5a138dc3c3077.jpg"  # Your photo URL
 
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
@@ -58,10 +58,35 @@ async def ctop(update: Update, context: CallbackContext) -> None:
         character_count = user['character_count']
         leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
     
-    photo_url = random.choice(PHOTO_URL)
+    photo_url = "https://telegra.ph/file/1d9c963d5a138dc3c3077.jpg"  # Your photo URL
 
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
+async def ctop(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+
+    cursor = group_user_totals_collection.aggregate([
+        {"$match": {"group_id": chat_id}},
+        {"$project": {"username": 1, "first_name": 1, "character_count": "$count"}},
+        {"$sort": {"character_count": -1}},
+        {"$limit": 10}
+    ])
+    leaderboard_data = await cursor.to_list(length=10)
+
+    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n"
+
+    for i, user in enumerate(leaderboard_data, start=1):
+        username = user.get('username', 'Unknown')
+        first_name = html.escape(user.get('first_name', 'Unknown'))
+
+        if len(first_name) > 10:
+            first_name = first_name[:15] + '...'
+        character_count = user['character_count']
+        leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
+    
+    photo_url = "https://telegra.ph/file/1d9c963d5a138dc3c3077.jpg"  # Your photo URL
+
+    await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
 async def leaderboard(update: Update, context: CallbackContext) -> None:
     
@@ -83,29 +108,27 @@ async def leaderboard(update: Update, context: CallbackContext) -> None:
         character_count = user['character_count']
         leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
     
-    photo_url = random.choice(PHOTO_URL)
+    photo_url = "https://telegra.ph/file/1d9c963d5a138dc3c3077.jpg"  # Your photo URL
 
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
 
 async def broadcast(update: Update, context: CallbackContext) -> None:
-    
+    OWNER_ID = '6069337486'  # Set the OWNER_ID directly within the function
+
     if str(update.effective_user.id) == OWNER_ID:
-        
         if update.message.reply_to_message is None:
             await update.message.reply_text('Please reply to a message to broadcast.')
             return
 
-        
         all_users = await user_collection.find({}).to_list(length=None)
         all_groups = await group_user_totals_collection.find({}).to_list(length=None)
-        
+
         unique_user_ids = set(user['id'] for user in all_users)
         unique_group_ids = set(group['group_id'] for group in all_groups)
 
         total_sent = 0
         total_failed = 0
-
 
         for user_id in unique_user_ids:
             try:
@@ -114,7 +137,6 @@ async def broadcast(update: Update, context: CallbackContext) -> None:
             except Exception:
                 total_failed += 1
 
-        
         for group_id in unique_group_ids:
             try:
                 await context.bot.forward_message(chat_id=group_id, from_chat_id=update.effective_chat.id, message_id=update.message.reply_to_message.message_id)
@@ -122,7 +144,6 @@ async def broadcast(update: Update, context: CallbackContext) -> None:
             except Exception:
                 total_failed += 1
 
-        
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=f'Broadcast report:\n\nTotal messages sent successfully: {total_sent}\nTotal messages failed to send: {total_failed}'
@@ -131,20 +152,46 @@ async def broadcast(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text('Only Murat Can use')
 
 
+async def broadcast2(update: Update, context: CallbackContext) -> None:
+    OWNER_ID = '6069337486'  # Set the OWNER_ID directly within the function
+
+    if str(update.effective_user.id) == OWNER_ID:
+        if update.message.reply_to_message is None:
+            await update.message.reply_text('Please reply to a message to broadcast.')
+            return
+
+        all_groups = await group_user_totals_collection.find({}).to_list(length=None)
+        unique_group_ids = set(group['group_id'] for group in all_groups)
+
+        total_sent = 0
+        total_failed = 0
+
+        for group_id in unique_group_ids:
+            try:
+                await context.bot.forward_message(chat_id=group_id, from_chat_id=update.effective_chat.id, message_id=update.message.reply_to_message.message_id)
+                total_sent += 1
+            except Exception:
+                total_failed += 1
+
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'Broadcast report:\n\nTotal messages sent successfully: {total_sent}\nTotal messages failed to send: {total_failed}'
+        )
+    else:
+        await update.message.reply_text('Only the owner can use this command for group broadcast.')
+
 async def stats(update: Update, context: CallbackContext) -> None:
-    
+    OWNER_ID = ['6069337486']  # Replace '123456789' with the actual owner ID
+
     if str(update.effective_user.id) not in OWNER_ID:
-        update.message.reply_text('only For Sudo users...')
+        await update.message.reply_text('Only for sudo users...')
         return
 
-    
     user_count = await user_collection.count_documents({})
 
+    group_count = len(await group_user_totals_collection.distinct('group_id'))
 
-    group_count = await group_user_totals_collection.distinct('group_id')
-
-
-    await update.message.reply_text(f'Total Users: {user_count}\nTotal groups: {len(group_count)}')
+    await update.message.reply_text(f'Total Users: {user_count}\nTotal Groups: {group_count}')
 
 
 
@@ -184,10 +231,28 @@ async def send_groups_document(update: Update, context: CallbackContext) -> None
         await context.bot.send_document(chat_id=update.effective_chat.id, document=f)
     os.remove('groups.txt')
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+async def brotu(update: Update, context: CallbackContext) -> None:
+    photo_url = 'https://telegra.ph/file/039b4488f7a8ff9c20bf2.jpg'  # Replace 'YOUR_PHOTO_URL_HERE' with your photo URL
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Add to Group", url='http://t.me/Dark_waifu_Bot?startgroup=new'),
+            InlineKeyboardButton("Support Group", url='https://t.me/dark_world_231')
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    # Send the photo with the inline keyboard and a message
+    await update.message.reply_photo(photo=photo_url, caption="𝖧𝖾𝗒 sukuna 𝗎𝗌𝖾𝗋𝗌, 𝗂𝖿 𝗒𝗈𝗎 𝗐𝖺𝗇𝗍 𝗍𝗈 𝖺𝖽𝖽 𝗒𝗈𝗎𝗋 𝖿𝖺𝗏𝗈𝗋𝗂𝗍𝖾 𝖼𝗁𝖺𝗋𝖺𝖼𝗍𝖾𝗋. 𝖸𝗈𝗎 𝖼𝖺𝗇 𝖺𝖽𝖽 𝖺𝗇𝗒 𝖼𝗁𝖺𝗋𝖺𝖼𝗍𝖾𝗋 𝗂𝗇 𝗍𝗁𝗂𝗌 𝗅𝗂𝗄𝖾 𝖠𝗇𝗂𝗆𝖾 , 𝖼𝖺𝗋𝗍𝗈𝗈𝗇 𝖺𝗇𝖽 𝖬𝖺𝗋𝗏𝖾𝗅 𝖾𝗍𝖼. 𝗒𝗈𝗎 𝖼𝖺𝗇 𝖽𝗆 𝗆𝖾 :- @sukuna201", reply_markup=reply_markup)
+
+application.add_handler(CommandHandler('brotu', brotu, block=False))
 application.add_handler(CommandHandler('ctop', ctop, block=False))
 application.add_handler(CommandHandler('stats', stats, block=False))
 application.add_handler(CommandHandler('TopGroups', global_leaderboard, block=False))
+application.add_handler(CommandHandler('broadcast2', broadcast2, block=False))
 
 application.add_handler(CommandHandler('list', send_users_document, block=False))
 application.add_handler(CommandHandler('groups', send_groups_document, block=False))
@@ -195,4 +260,3 @@ application.add_handler(CommandHandler('groups', send_groups_document, block=Fal
 
 application.add_handler(CommandHandler('top', leaderboard, block=False))
 application.add_handler(CommandHandler('broadcast', broadcast, block=False))
-
